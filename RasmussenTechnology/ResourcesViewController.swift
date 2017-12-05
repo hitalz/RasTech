3e419a42fe5234170d068d99f9df7c83605af097
hitalz <hitalz@icloud.com>
2017-12-05 14:56:47 -0600

    Corrected some constraints and added some comments

--- RasmussenTechnology/ResourcesViewController.swift
+++ RasmussenTechnology/ResourcesViewController.swift
@@ -10,28 +10,40 @@ import UIKit
 import SafariServices
 
 class ResourcesViewController: UIViewController {
-
+    @IBOutlet weak var portalButton: UIButton!
+    @IBOutlet weak var blackboardButton: UIButton!
+    @IBOutlet weak var resourcesButton: UIButton!
+    @IBOutlet weak var answersButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //Customize buttons
+        portalButton.layer.cornerRadius = 10
+        blackboardButton.layer.cornerRadius = 10
+        resourcesButton.layer.cornerRadius = 10
+        answersButton.layer.cornerRadius = 10
     }
 
+    //Open Safari in app to display URL
     @IBAction func studentPortalButton(_ sender: Any) {
         let webPage = SFSafariViewController(url: URL(string: "https://portal.rasmussen.edu")!)
         self.present(webPage, animated: true, completion: nil)
     }
     
+    //Open Safari in app to display URL
     @IBAction func blackboardButton(_ sender: Any) {
         let webPage = SFSafariViewController(url: URL(string: "https://learning.rasmussen.edu")!)
         self.present(webPage, animated: true, completion: nil)
     }
     
+    //Open Safari in app to display URL
     @IBAction func resourcesButton(_ sender: Any) {
         let webPage = SFSafariViewController(url: URL(string: "http://guides.rasmussen.edu/technology")!)
         self.present(webPage, animated: true, completion: nil)
     }
     
+    //Open Safari in app to display URL
     @IBAction func answersButton(_ sender: Any) {
         let webPage = SFSafariViewController(url: URL(string: "https://rasmussen.libanswers.com")!)
         self.present(webPage, animated: true, completion: nil)
